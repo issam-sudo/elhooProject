b56a966c20825b98606b5b9ab39b408f56cec150
issam-sudo <issambenkaroum95@gmail.com>
2020-08-02 22:34:44 +0100

    545

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TestComponent } from './test/test.component';
+
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'test', component: TestComponent },
+  { path: '**', redirectTo: 'dashboard' , pathMatch: 'full'}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
+import { DashbordMenuComponent } from './dashbord-menu/dashbord-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatDatepickerModule  } from '@angular/material/datepicker';
+import {  MatNativeDateModule } from '@angular/material/core';
+import { TestComponent } from './test/test.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NavComponent,
+    DashbordMenuComponent,
+    TestComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
